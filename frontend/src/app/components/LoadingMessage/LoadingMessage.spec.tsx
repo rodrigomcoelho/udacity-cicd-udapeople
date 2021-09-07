97f7fc4410d290c3a867c84ee18257f2f8475699
Rodrigo Coelho <rodrigo.coelho@hotmail.com.br>
2021-09-07 14:28:35 -0400

    first commit

---
+++ frontend/src/app/components/LoadingMessage/LoadingMessage.spec.tsx
@@ -0,0 +1,15 @@
+import { LoadingMessage } from 'app/components/LoadingMessage';
+import { shallow } from 'enzyme';
+import * as React from 'react';
+
+describe('<LoadingMessage>', () => {
+  describe('Props', () => {
+    describe('message', () => {
+      it('Should render the props message', () => {
+        const message = 'Hello!';
+        const wrapper = shallow(<LoadingMessage message={message} />);
+        expect(wrapper.contains(<span>{message}</span>)).toBeTruthy(); //remove the question mark to make the test pass
+      });
+    });
+  });
+});
